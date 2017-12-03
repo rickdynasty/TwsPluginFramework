54bab18c19482d3e42b7544d4e239db6b7aa0ce1
rick_tan <rick_tan@qq.com>
2017-12-03 20:59:52 +0800

    Demo 不能直接依赖shareLib，当前只能通过jar的provided的方式依赖

--- TwsPluginDemo/src/main/java/com/example/plugindemo/MainActivity.java
+++ TwsPluginDemo/src/main/java/com/example/plugindemo/MainActivity.java
@@ -3,11 +3,18 @@ package com.example.plugindemo;
 import android.app.Activity;
 import android.os.Bundle;
 
+import com.rick.tws.framework.HostProxy;
+
+import qrom.component.log.QRomLog;
+
 public class MainActivity extends Activity {
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        QRomLog.d(TAG, "onCreate host pkg:" + HostProxy.getApplication().getPackageName());
     }
 }

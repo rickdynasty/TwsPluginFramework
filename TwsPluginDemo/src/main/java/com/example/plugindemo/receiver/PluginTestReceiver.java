e31b395d4dbc8c92583d60027888e340a65b4d06
rick_tan <rick_tan@qq.com>
2017-12-05 09:55:00 +0800

    区分一下两个receiver的action 方便测试

--- TwsPluginDemo/src/main/java/com/example/plugindemo/receiver/PluginTestReceiver.java
+++ TwsPluginDemo/src/main/java/com/example/plugindemo/receiver/PluginTestReceiver.java
@@ -22,7 +22,7 @@ public class PluginTestReceiver extends BroadcastReceiver {
 
 		Log.d("PluginTestReceiver", ((ParamVO) intent.getSerializableExtra("paramvo")) + ", action:" + intent.getAction());
 
-		Toast.makeText(context, "PluginTestReceiver onReceive " + context.getResources().getText(R.string.hello_world4),
+		Toast.makeText(context, "PluginTestReceiver onReceive1 " + context.getResources().getText(R.string.hello_world4),
 				Toast.LENGTH_SHORT).show();
 	}
 

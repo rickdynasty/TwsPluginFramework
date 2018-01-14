e9298fcce15c85f87403435d57299a7d7c2d4241
rick_tan <rick_tan@qq.com>
2018-01-14 21:54:26 +0800

    update

--- TwsPluginShareLib/src/main/java/com/rick/tws/app/BaseFragment.java
+++ TwsPluginShareLib/src/main/java/com/rick/tws/app/BaseFragment.java
@@ -6,5 +6,5 @@ import android.support.v4.app.Fragment;
  * Created by Administrator on 2018/1/14 0014.
  */
 
-public class TwsFragment extends Fragment {
+public class BaseFragment extends Fragment {
 }
